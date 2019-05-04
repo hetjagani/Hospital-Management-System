623e32ee90e1bca4914f285bfb7c2cbb5a24afc8
hetjagani <hetpatel572@gmail.com>
2019-05-04 12:49:28 +0530

    adding nurse func

--- nurse_menu.php
+++ nurse_menu.php
@@ -1,3 +1,14 @@
+<?php
+  require 'connect.php';
+  session_start();
+  $nurse_id = $_SESSION['nurse_id'];
+
+  //get nurse name
+  $nurse_name_q = 'SELECT * FROM Nurse JOIN Person USING(PersonId) WHERE NurseId = "'.$nurse_id.'";';
+  $nurse_name_run = mysqli_query($conn, $nurse_name_q) or die(mysqli_error($conn));
+  $nurse_data = mysqli_fetch_assoc($nurse_name_run);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,47 +19,58 @@
 </head>
 <body>
 <center>
-  <h2> Nurse </h2>
+  <h2> Nurse <?php echo '['.$nurse_data['FirstName'].' '.$nurse_data['LastName'].']'; ?></h2>
   <hr>
-  <form action="" method="GET">
+  <form action="" method="POST">
     Enter Date : <input type="Date" name="n_date"> 
     <input type="submit" value="Search">
   </form>
   <hr>
-  <table border="1px">
+  
+  <table border>
     <tr>
+      <td> Patient Name </td>
       <td> Room No </td>
       <td> Bed No </td>
+      <td> Gender </td>
+      <td> Date of Birth </td>
+      <td> Contact No </td>
     </tr>
-    <tr>
-      <td> 12 </td>
-      <td> 10 </td>
-    </tr>
-  </table>
-  <hr>
-  <h2>Surgery Details</h2>
-  <table>
-    <tr>
-      <td> Surgery No :  </td>
-      <td> XX </td>
-    </tr>
-    <tr>
-      <td> Doctor Name : </td>
-      <td> XXX </td>
-    </tr>
-    <tr>
-      <td> Room no : </td>
-      <td> XXX </td>
-    </tr>
-    <tr>
-      <td>Patient Name : </td>
-      <td> XXX </td>
-    </tr>
-    <tr>
-      <td> Assistant Doctor Name : </td>
-      <td> XXX </td>
-    </tr>
-  </table>
+  <?php
+    $flag = 0;
+    if(isset($_POST)) {
+      $serve_query = 'SELECT * FROM Serve WHERE Date = "'.$_POST['n_date'].'" AND NurseId = "'.$nurse_id.'";';
+      $serve_run = mysqli_query($conn, $serve_query) or die(mysqli_error($conn));
+      
+      while($serve_data = mysqli_fetch_assoc($serve_run)) {
+        $patient_name_q = 'SELECT * FROM (IPDAppointment JOIN Patient USING(PatientId)) JOIN Person USING(PersonId) WHERE IPDAppointmentId = "'.$serve_data['IPDAppointmentId'].'"';
+        $patient_name_run = mysqli_query($conn, $patient_name_q) or die(mysqli_error($conn));
+        $patient_details = mysqli_fetch_assoc($patient_name_run);
+
+        if ($patient_details['Gender'] == 0) {
+          $gender = 'Male';
+        } elseif ($patient_details['Gender'] == 1) {
+          $gender = 'Female';
+        }
+
+        echo '<tr>
+                <td> '.$patient_details['FirstName'].' '.$patient_details['LastName'].'</td>
+                <td> '.$serve_data['RoomNo'].' </td>
+                <td> '.$serve_data['BedNo'].' </td>
+                <td> '.$gender.' </td>
+                <td> '.$patient_details['DateOfBirth'].' </td>
+                <td> '.$patient_details['ContactNo'].' </td>
+              </tr>
+            ';
+        $flag = 1;
+      }
+    }
+    if ($flag === 0) {
+      echo '<h3>No Patient Assigned...</h3>';
+    }
+  ?>
+
+    </table>
     <hr>
     <a href="index.php"> LOGOUT </a> <br>
     <hr>
